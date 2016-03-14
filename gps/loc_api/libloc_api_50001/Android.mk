0874a09f7de0aa56165738446eb71e86526ae1ea
Saurabh Srivastava <ssrivast@codeaurora.org>
2016-03-14 15:30:09 +0530

    Enabling CLANG compilation and fixing all resulting errors
    Removing statement to set LOCAL_CLANG flag explicity to
    true. It will be true by default.
    
    Change-Id: I2eaba5a89e64088e3383b962dceaaa7e975e997a
    CRs-Fixed: 989476

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -38,10 +38,15 @@ LOCAL_CFLAGS += \
      -fno-short-enums \
      -D_ANDROID_
 
+ifeq ($(QCPATH),)
+LOCAL_CFLAGS += -DOSS_BUILD
+endif
+
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
     $(TARGET_OUT_HEADERS)/libloc_core \
-    $(LOCAL_DIR)
+    $(LOCAL_PATH) \
+    $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_COPY_HEADERS_TO:= libloc_eng/
 LOCAL_COPY_HEADERS:= \
@@ -76,6 +81,9 @@ LOCAL_SHARED_LIBRARIES := \
     libgps.utils \
     libdl
 
+ifneq ($(filter $(TARGET_DEVICE), apq8084 msm8960), false)
+endif
+
 LOCAL_SRC_FILES += \
     loc.cpp \
     gps.c
@@ -88,13 +96,18 @@ ifeq ($(TARGET_USES_QCOM_BSP), true)
 LOCAL_CFLAGS += -DTARGET_USES_QCOM_BSP
 endif
 
+ifeq ($(QCPATH),)
+LOCAL_CFLAGS += -DOSS_BUILD
+endif
+
 ## Includes
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    $(TARGET_OUT_HEADERS)/libloc_core
+    $(TARGET_OUT_HEADERS)/libloc_core \
+    $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 include $(BUILD_SHARED_LIBRARY)
 

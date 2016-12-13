CyanogenMod/android_device_asus_msm8916-common

msm8916-common: Add shim for libwvm.so
Change-Id: I1ac58a108783ae1aabe3f3f061b2444925f684db

libshims/Android.mk
@@ -57,3 +57,14 @@ LOCAL_C_INCLUDES += \
 LOCAL_MODULE:= libshim_camera
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := wvm.c
+
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+
+LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

nickdoherty81/android_device_motorola_msm8916-common

libshims/Android.mk
@@ -25,3 +25,14 @@ LOCAL_SHARED_LIBRARIES := libutils libgui liblog libbinder
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := wvm.c
+
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+
+LOCAL_MODULE := libshims_wvm
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)